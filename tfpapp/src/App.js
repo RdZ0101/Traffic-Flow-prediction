480acc9ea9e425c2f8d0d8dc38580d65b4a19c3a
Bentaylornz1 <128343276+Bentaylornz1@users.noreply.github.com>
2024-10-25 17:36:30 +1100

    GUI and neighbors fixed

--- tfpapp/src/App.js
+++ tfpapp/src/App.js
@@ -18,48 +18,49 @@ const MyMap = () => {
   const [statusMessage, setStatusMessage] = useState('');
   const [pathCoordinates, setPathCoordinates] = useState([]);
   const [pathCost, setPathCost] = useState(null);
+  const [pathLabels, setPathLabels] = useState([]);
 
   const positionsWithLabels = [
-    { position: [-37.865849, 145.09282], label: '970' },
-    { position: [-37.850381, 145.094563], label: '2000' },
-    { position: [-37.815031, 145.09938], label: '2200' },
-    { position: [-37.792949, 145.03148], label: '2820' },
-    { position: [-37.78661, 145.06202], label: '2825' },
-    { position: [-37.784762, 145.006304], label: '2827' },
-    { position: [-37.811399, 145.00983], label: '2846' },
-    { position: [-37.8145649, 145.02221], label: '3001' },
-    { position: [-37.813661, 145.02785], label: '3002' },
-    { position: [-37.822272, 145.06570], label: '3120' },
-    { position: [-37.8813661, 145.05858], label: '3122' },
-    { position: [-37.822272, 145.09994], label: '3126' },
-    { position: [-37.823725, 145.07922], label: '3127' },
-    { position: [-37.794653, 145.08487], label: '3180' },
-    { position: [-37.807452, 145.02895], label: '3662' },
-    { position: [-37.836009, 145.09821], label: '3682' },
-    { position: [-37.853330, 145.09512], label: '3685' },
-    { position: [-37.832129, 145.06367], label: '3804' },
-    { position: [-37.836016, 145.06217], label: '3812' },
-    { position: [-37.793422, 145.06217], label: '4030' },
-    { position: [-37.800782, 145.06253], label: '4032' },
-    { position: [-37.810383, 145.06071], label: '4034' },
-    { position: [-37.817341, 145.05926], label: '4035' },
-    { position: [-37.810383, 145.05668], label: '4040' },
-    { position: [-37.845688, 145.05396], label: '4043' },
-    { position: [-37.792532, 145.07044], label: '4051' },
-    { position: [-37.8049687, 145.0817598],label: '4057' },
-    { position: [-37.8143725, 145.080005], label: '4063' },
-    { position: [-37.82155, 145.01503], label: '4262' },
-    { position: [-37.8230562, 145.024958], label: '4263' },
-    { position: [-37.8241054, 145.0340062], label: '4264' },
-    { position: [-37.82529, 145.04387], label: '4266' },
-    { position: [-37.8302275, 145.032815], label: '4270' },
-    { position: [-37.8318833, 145.0463933], label: '4272' },
-    { position: [-37.84659, 145.04443], label: '4273' },
-    { position: [-37.8008379, 145.049119], label: '4321' },
-    { position: [-37.8093323, 145.0368477], label: '4324' },
-    { position: [-37.806215, 145.03525], label: '4335' },
-    { position: [-37.8289133, 145.0155867], label: '4812' },
-    { position: [-37.812965, 145.00847], label: '4821' }
+    { position: [-37.865704, 145.092782], label: '970' },
+    { position: [-37.850432, 145.095693], label: '2000' },
+    { position: [-37.815014, 145.099383], label: '2200' },
+    { position: [-37.792898, 145.031484], label: '2820' },
+    { position: [-37.784762, 145.063002], label: '2825' },
+    { position: [-37.780186, 145.078652], label: '2827' },
+    { position: [-37.860067, 145.059063], label: '2846' },
+    { position: [-37.813200, 145.023426], label: '3001' },
+    { position: [-37.813657, 145.027884], label: '3002' },
+    { position: [-37.821402, 145.058585], label: '3120' },
+    { position: [-37.822289, 145.065691], label: '3122' },
+    { position: [-37.826262, 145.099918], label: '3126' },
+    { position: [-37.823763, 145.079242], label: '3127' },
+    { position: [-37.794666, 145.084869], label: '3180' },
+    { position: [-37.807465, 145.028910], label: '3662' },
+    { position: [-37.835996, 145.098209], label: '3682' },
+    { position: [-37.853343, 145.095114], label: '3685' },
+    { position: [-37.832150, 145.063699], label: '3804' },
+    { position: [-37.836024, 145.062179], label: '3812' },
+    { position: [-37.793464, 145.063967], label: '4030' },
+    { position: [-37.800795, 145.062529], label: '4032' },
+    { position: [-37.810383, 145.060690], label: '4034' },
+    { position: [-37.817358, 145.059325], label: '4035' },
+    { position: [-37.831383, 145.056684], label: '4040' },
+    { position: [-37.845713, 145.053974], label: '4043' },
+    { position: [-37.792524, 145.070455], label: '4051' },
+    { position: [-37.803506, 145.083143],label: '4057' },
+    { position: [-37.812843, 145.081336], label: '4063' },
+    { position: [-37.820108, 145.016480], label: '4262' },
+    { position: [-37.821537, 145.026401], label: '4263' },
+    { position: [-37.822586, 145.035347], label: '4264' },
+    { position: [-37.823743, 145.044788], label: '4266' },
+    { position: [-37.828746, 145.034231], label: '4270' },
+    { position: [-37.830336, 145.047684], label: '4272' },
+    { position: [-37.845437, 145.044991], label: '4273' },
+    { position: [-37.799329, 145.050664], label: '4321' },
+    { position: [-37.807709, 145.038152], label: '4324' },
+    { position: [-37.804952, 145.035883], label: '4335' },
+    { position: [-37.827277, 145.017032], label: '4812' },
+    { position: [-37.811416, 145.009825], label: '4821' }
   ];
 
   const handleMarkerClick = (label, position) => {
@@ -80,6 +81,7 @@ const MyMap = () => {
     setPathCost(null);
     setSelecting(true);
     setStatusMessage('Please select your start point.');
+    setPathLabels([]);
   };
 
   const handleSubmit = async () => {
@@ -100,12 +102,16 @@ const MyMap = () => {
         console.log('Path Cost:', data.path_cost);
         console.log('Path:', data.path);
 
+        setPathCost(data.path_cost);
+
         // Set the path cost and map SCATS IDs to positions
         setPathCost(data.path_cost);
         const newPathCoordinates = data.path.map(scatsId => {
           const match = positionsWithLabels.find(item => item.label === scatsId.toString());
           return match ? match.position : null;
         }).filter(Boolean);
+
+        setPathLabels(data.path);
 
         setPathCoordinates(newPathCoordinates);
       } catch (error) {
@@ -128,6 +134,15 @@ const MyMap = () => {
         <button onClick={handleJourneyStart}>Start Journey</button>
         {start && target && <button onClick={handleSubmit}>Submit</button>}
         <p>{statusMessage}</p>
+
+        {pathLabels.length > 0 && (
+          <div>
+            <strong>Route:</strong> {pathLabels.join(' >> ')}
+            <div>
+              <strong>Total Travel Time:</strong> {(pathCost / 60).toFixed(2)} minutes
+            </div>
+          </div>
+        )}
       </div>
 
       <MapContainer center={[-37.8238567, 145.0643933]} zoom={13} scrollWheelZoom={false} style={{ height: '100vh', width: '100%' }}>
